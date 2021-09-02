1603271401a377a340f3e13d710dfd32dbd3569a
anapperez <anaportillo.pai@gmail.com>
2021-09-02 15:05:53 -0400

    add more buttons and reset butons

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// this is where the brain lives
+// this is where the brain lives!!
 
-let count = 0; // First Variable! 
+let count = 0; // First Variable!!
 
 function appleCounter() {
   count = count + 1;
@@ -14,22 +14,23 @@ function pearCounter() {
   document.getElementById("pear").innerHTML = count2;
 }
 
-let count3 = 0; // Third Variable!
+let count3 = 0; // Third Variable!!
 
 function strawberryCounter() {
   count3 = count3 + 1;
   document.getElementById("strawberry").innerHTML = count3;
 }
-  let count4 = 0; // Fourth Variable!
+  let count4 = 0; // Fourth Variable!!
 
   function potatoCounter() {
     count4 = count4 + 1;
     document.getElementById("potato").innerHTML = count4;
   }
 
-  let count5 = 0; // Fifth Variable!
+  let count5 = 0; // Fifth Variable!!
 
   function tomatoCounter() {
     count5 = count5 + 1;
     document.getElementById("tomato").innerHTML = count5;
   }
+
