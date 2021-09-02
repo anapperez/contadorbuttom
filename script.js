d5d857ccf673c6c031dca9cd91c8b8ef34fd93b8
anapperez <anaportillo.pai@gmail.com>
2021-09-02 14:54:54 -0400

    add more buttons and reset button

--- script.js
+++ script.js
@@ -7,3 +7,29 @@ function appleCounter() {
  document.getElementById("apple").innerHTML = count;
 }
  
+let count2 = 0; // Second Variable!
+
+function pearCounter() {
+  count2 = count2 + 1;
+  document.getElementById("pear").innerHTML = count2;
+}
+
+let count3 = 0; // Third Variable!
+
+function strawberryCounter() {
+  count3 = count3 + 1;
+  document.getElementById("strawberry").innerHTML = count3;
+}
+  let count4 = 0; // Fourth Variable!
+
+  function potatoCounter() {
+    count4 = count4 + 1;
+    document.getElementById("potato").innerHTML = count4;
+  }
+
+  let count5 = 0; // Fifth Variable!
+
+  function tomatoCounter() {
+    count5 = count5 + 1;
+    document.getElementById("tomato").innerHTML = count5;
+  }
